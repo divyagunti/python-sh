9153ceeb623b647831f7e954ffba28f262d3eaf1
divyagunti <129255866+divyagunti@users.noreply.github.com>
2023-05-29 21:50:50 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,6 @@
-sudo yum -y update
-sudo yum -y install python3-pip
-sudo yum -y install git
+sudo apt update
+sudo apt -y upgrade
+sudo apt -y install python3-pip
 sudo git clone https://github.com/Farooq8090/USA-Housing.git
 cd USA-Housing
 sudo pip3 install -r requirements.txt
