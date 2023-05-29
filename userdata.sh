ae4350acd1c8c18b21a87404bfbd4a23c4657f43
divyagunti <129255866+divyagunti@users.noreply.github.com>
2023-05-29 21:54:26 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt -y upgrade
 sudo apt -y install python3-pip
-sudo git clone https://github.com/Farooq8090/USA-Housing.git
+sudo git clone https://github.com/divyagunti/USA-Housing.git
 cd USA-Housing
 sudo pip3 install -r requirements.txt
 screen -m -d python3 app.py
